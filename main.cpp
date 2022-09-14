b7573f34ca091ed38a0e0022e83cd6a546b438b1
Yelodress <thomasdouence@gmail.com>
2022-09-14 22:37:59 +0200

    switchcasev1

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int etlogique();
+
+int main()
+{
+    char choix;
+    cin>>choix;
+    switch (choix)
+    {
+        case 'a':
+            //appel fonction calcul ip en binaire
+            cout<<"a saisi"<<endl;
+            break;
+        case 'b':
+            //appel fonction et logique
+            etlogique();
+            break;
+        case 'c':
+            //appel fonction ou logique
+            break;
+
+    }
+}
+
+int etlogique() {
+    int binip[32];
+    int binmasque[32];
+    int resultat[32];
+    int rang=0;
+
+    for (int tour=0; tour<32; tour++)
+    {
+        binip[rang]=rang;
+        cin>>binip[rang];
+        cout<<binip[rang];
+        rang++;
+
+    }
+    return resultat[rang];
+};
